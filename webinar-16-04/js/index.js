54b49edb8bc193460627ac54abea728e099dca71
Alexander Repeta <axzerk@gmail.com>
2018-04-16 22:18:35 +0300

    adds webinar 16-04

---
+++ webinar-16-04/js/index.js
@@ -0,0 +1,118 @@
+'use strict';
+
+
+
+// const name = prompt('Plz enter your name', '');
+
+// const message = 'Welcome ' + name;
+
+// console.log(message);
+
+// const message = 'WELCOME';
+
+// const inLowerCase = message.toLowerCase();
+
+// console.log( inLowerCase );
+
+// console.log(message);
+
+
+// console.log( 'hello'.toUpperCase() );
+
+
+// const message = 'Welcome to hell to!'
+
+// console.log( message.includes('to') );
+
+// console.log( message.lastIndexOf('to') );
+
+// const name = 'Mango';
+
+// const mood = 'happy';
+
+// const message = name + ' is ' + mood;
+
+// 'Mango is happy'
+
+// console.log(message);
+
+// console.log( 5 + 5 + '2'  ); // '102'
+
+// const margin = '15usd';
+
+// console.log( Number.parseInt(margin) );
+
+// console.log( Number.parseFloat(margin) );
+
+// const str = Number('5qwe');
+
+// 5 + 2
+// +2
+
+// NaN - Not a Number
+// console.log(str);
+
+// const age = Number(prompt('get age', ''));
+
+// console.log(age);
+
+// console.log( Number.isNaN(age) );
+
+// console.log( typeof age === "number" );
+
+// const a = 0.27;
+// const b = 0.34;
+
+// const value = (a * 100 + b * 100) / 100;
+
+// console.log(value);
+
+// const message = 'I\'m \\ happy!';
+
+// console.log(message);
+
+// const type = typeof 5;
+
+// "number"
+// "string"
+// "boolean"
+// "undefined"
+
+// console.log('Variable type: ', type);
+
+// const userName = 'Mango';
+
+// alert('Привет' + userName);
+
+// const userAge = prompt('Введите ваш возраст!', '');
+
+// const x = 5 + 2 * 10;
+
+// console.log(x);
+
+// console.log(typeof userAge);
+
+// const isGoing = confirm('Вы хотите поехать на шашлыки?');
+
+// console.log(isGoing);
+
+// const isActive = false;
+
+// const hasChildren = true;
+
+// runtime время исполнения
+
+// let userAge = 18;
+
+// console.log(userAge);
+
+// userAge = 19;
+
+// console.log(userAge);
+
+// const name = 'Mango';
+
+// console.log(name);
+
+// имя-объекта.метод(параметры)
+// console.log('user age: ', userAge);
